fcd9f9c761e2969a6db6b662e64cdf58f6faa403
jpernia101 <jeffreypernia@gmail.com>
2024-12-28 19:47:51 -0600

    Some UI changes

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -21,53 +21,56 @@ const Home = () => {
                     </Col>
                     <Col style={{textAlign:'center'}}><h5>I'm a software engineer who currently resides in HTX.</h5></Col>
                     
-                    <Col id="about" style={{marginTop:'50px'}}>
+                    <Col id="about" style={{marginTop:'50px', backgroundColor: 'rgb(245 183 155)'}}>
                         <h3 className='section-style'>About Me</h3>
                         <p> I'm currently a software engineer at <b>JP Morgan</b> who enjoys learning and building scalable
                         apps. Also, I like to travel and explore. 
                         </p>
 
                     </Col>
-                    <h3 className='section-style'>Languages/Frameworks I am familiar with</h3>
-                    <Row >
-                     <Col xs={7}><FontAwesomeIcon  beatFade icon={faMinus} />Node/Express</Col>
-                     <Col><FontAwesomeIcon  beatFade icon={faMinus} />ReactJS</Col>
-                     
-                    </Row>
-                    <Row>
-                        <Col xs={7}><FontAwesomeIcon  beatFade icon={faMinus} />Java</Col>
-                        <Col><FontAwesomeIcon  beatFade icon={faMinus} />Python</Col>
-                    </Row>
-                    <Row>
-                        <Col xs={7}><FontAwesomeIcon  beatFade icon={faMinus} />AWS(S3, Lambda, EC2, Step-Functinos)</Col>
-                        <Col><FontAwesomeIcon  beatFade icon={faMinus} />GraphQL</Col>
-                    </Row>
+                    <div style={{marginTop:'50px', backgroundColor: 'rgb(245 183 155)'}}>
+                        <h3 className='section-style'>Languages/Frameworks I am familiar with</h3>
+                        <Row >
+                        <Col xs={7}><FontAwesomeIcon  beatFade icon={faMinus} />Node/Express</Col>
+                        <Col><FontAwesomeIcon  beatFade icon={faMinus} />ReactJS</Col>
+                        
+                        </Row>
+                        <Row>
+                            <Col xs={7}><FontAwesomeIcon  beatFade icon={faMinus} />Java</Col>
+                            <Col><FontAwesomeIcon  beatFade icon={faMinus} />Python</Col>
+                        </Row>
+                        <Row>
+                            <Col xs={7}><FontAwesomeIcon  beatFade icon={faMinus} />AWS(S3, Lambda, EC2, Step-Functions)</Col>
+                            <Col><FontAwesomeIcon  beatFade icon={faMinus} />GraphQL</Col>
+                        </Row>
+                    </div>
                     
-                    <Col id='travel' style={{marginTop:'50px'}}>
+                    
+                    <Col id='travel' style={{marginTop:'50px', backgroundColor: 'rgb(245 183 155)'}}>
                         <h3 className='section-style'>Travel Photos</h3>
                         <Carousel interval={null}>
                             <Carousel.Item>
-                                <Image  className='carousel-image' src='/denver.JPEG'/>
+                                <Image  roundedCircle className='carousel-image' src='/denver.JPEG'/>
                                 <Carousel.Caption><p className='carousel-caption-p'> Denver </p></Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
-                                <Image  className='carousel-image' src='/denver2.JPEG'/>
+                                <Image  roundedCircle className='carousel-image' src='/denver2.JPEG'/>
                                 <Carousel.Caption><p className='carousel-caption-p'> Denver </p></Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
-                                <Image className='carousel-image' src='/denver3.JPEG'/>
+                                <Image roundedCircle className='carousel-image' src='/denver3.JPEG'/>
                                 <Carousel.Caption><p className='carousel-caption-p'> Denver </p></Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
-                                <Image  className='carousel-image' src='/DR.JPEG'/>
+                                <Image roundedCircle className='carousel-image' src='/DR.JPEG'/>
                                 <Carousel.Caption><p className='carousel-caption-p'> Dominican Republic </p></Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
-                                <Image  className='carousel-image' src='/DR2.JPEG'/>
+                                <Image roundedCircle className='carousel-image' src='/DR2.JPEG'/>
                                 <Carousel.Caption><p className='carousel-caption-p'> Dominican Republic </p></Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
-                                <Image  className='carousel-image' src='/jamaica.JPEG'/>
+                                <Image roundedCircle className='carousel-image' src='/jamaica.JPEG'/>
                                 <Carousel.Caption><p className='carousel-caption-p'> Jamaica </p></Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
@@ -75,14 +78,14 @@ const Home = () => {
                                 <Carousel.Caption><p className='carousel-caption-p'> Nashville </p></Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
-                                <Image  className='carousel-image' src='/ny.JPEG'/>
+                                <Image roundedCircle className='carousel-image' src='/ny.JPEG'/>
                                 <Carousel.Caption><p className='carousel-caption-p'> New York</p></Carousel.Caption>
                             </Carousel.Item>
                             <Carousel.Item>
-                                <Image  className='carousel-image' src='/ny2.JPEG'/>
+                                <Image roundedCircle className='carousel-image' src='/ny2.JPEG'/>
                                 <Carousel.Caption><p className='carousel-caption-p'> New York </p></Carousel.Caption>
                             </Carousel.Item><Carousel.Item>
-                                <Image  className='carousel-image' src='/ny3.JPEG'/>
+                                <Image roundedCircle className='carousel-image' src='/ny3.JPEG'/>
                                 <Carousel.Caption><p className='carousel-caption-p'> New York </p></Carousel.Caption>
                             </Carousel.Item>
 
